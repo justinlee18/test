1cda1aa682bfdb2fd4ab4699b4798a782c8de6cb
Justin Lee <justinlee18@gmail.com>
2018-09-30 22:25:36 -0400

    3rd change

--- prime.js
+++ prime.js
@@ -1,3 +1,13 @@
-var prime = function(num){
-    for(let i=0; i<num.lenet)
-}
+function multi_array_sum(arr) {
+    var sum = 0;
+    for(var i = 0; i < arr.length; i++) {
+     if(Array.isArray(arr[i])) {
+       sum += multi_array_sum(arr[i]);
+     } else {
+       sum += arr[i];
+     }
+    }
+    return sum;
+  }
+
+  console.log(multi_array_sum([1,2,3,4,5]))
